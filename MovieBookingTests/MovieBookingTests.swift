180ab065e8b6da59757c7004ae19c89c8c7057dd
minneee <alsgml022100@naver.com>
2025-10-13 19:34:21 +0900

    Initial Commit

---
+++ MovieBookingTests/MovieBookingTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingTests.swift
+//  MovieBookingTests
+//
+//  Created by 김민희 on 10/13/25.
+//
+
+import Testing
+@testable import MovieBooking
+
+struct MovieBookingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
