7665847e831a4e952236008f2afe0a37650699d9
minneee <alsgml022100@naver.com>
2025-10-14 15:22:02 +0900

    [feat] 영화 리스트 UI 구현

---
+++ MovieBooking/Feature/MovieList/Components/MovieCardView.swift
@@ -0,0 +1,33 @@
+//
+//  MovieCardView.swift
+//  MovieBooking
+//
+//  Created by 김민희 on 10/14/25.
+//
+
+import SwiftUI
+
+struct MovieCardView: View {
+  var body: some View {
+    VStack(alignment: .leading, spacing: 10) {
+      Image("movie")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 150, height: 200)
+        .background(.cyan)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(radius: 3, y: 3)
+
+      Text("Movie Title")
+        .font(.system(size: 16))
+        .frame(width: 150, alignment: .leading)
+        .lineLimit(1)
+
+      StarRatingView(rating: 3)
+    }
+  }
+}
+
+#Preview {
+  MovieCardView()
+}
---
+++ MovieBooking/Feature/MovieList/Components/StarRatingView.swift
@@ -0,0 +1,24 @@
+//
+//  StarRatingView.swift
+//  MovieBooking
+//
+//  Created by 김민희 on 10/14/25.
+//
+
+import SwiftUI
+
+struct StarRatingView: View {
+  let rating: Int
+
+  var body: some View {
+    HStack(spacing: 4) {
+      ForEach(1...5, id: \.self) { index in
+        Image(systemName: "star.fill")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 12, height: 12)
+          .foregroundColor(index <= rating ? .yellow : .gray)
+      }
+    }
+  }
+}
