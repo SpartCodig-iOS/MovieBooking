77e2cc155ef5ea80eb8e92072c580e3351d48207
Roy <shuwj81@daum.net>
2025-10-14 10:58:52 +0900

    [feat]: di 설정 #1

---
+++ MovieBooking/App/Di/DIRegistry.swift
@@ -0,0 +1,36 @@
+//
+//  DIRegistry.swift
+//  MovieBooking
+//
+//  Created by Wonji Suh  on 10/14/25.
+//
+
+import WeaveDI
+
+/// 모든 의존성을 자동으로 등록하는 레지스트리
+extension WeaveDI.Container {
+  private static let helper = RegisterModule()
+
+  ///  Repository 등록
+  static func registerRepositories() async {
+    let repositories: [ Module] = [
+
+    ]
+
+    await repositories.asyncForEach { module in
+      await module.register()
+    }
+  }
+
+  ///  UseCase 등록
+  static func registerUseCases() async {
+
+    let useCases: [Module] = [
+
+    ]
+
+    await useCases.asyncForEach { module in
+      await module.register()
+    }
+  }
+}
---
+++ MovieBooking/App/Di/Extension+AppDIContainer.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+AppDIContainer.swift
+//  MovieBooking
+//
+//  Created by Wonji Suh  on 10/14/25.
+//
+
+import WeaveDI
+
+extension AppWeaveDI.Container {
+  @DIContainerActor
+  func registerDefaultDependencies() async {
+    await registerDependencies(logLevel: .all) { container in
+      // Repository 먼저 등록
+      let factory = ModuleFactoryManager()
+
+      await factory.registerAll(to: container)
+    }
+  }
+}
+
