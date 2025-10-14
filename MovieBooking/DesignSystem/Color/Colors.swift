f1ac46cb0e2fe99a15c25f96326a7701c0db7681
Roy <shuwj81@daum.net>
2025-10-14 11:01:32 +0900

    [feat]: 디자인 시스템 컬러 및  폰트 구현 #1

---
+++ MovieBooking/DesignSystem/Color/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  MovieBooking
+//
+//  Created by Wonji Suh  on 10/14/25.
+//
+
+import SwiftUI
+
+public extension ShapeStyle where Self == Color {
+  static var deepViolet: Color { .init(hex: "#503396") }
+  static var royalIndigo: Color { .init(hex: "#503396") }
+  static var basicPurple: Color { .init(hex: "#503396") }
+}
