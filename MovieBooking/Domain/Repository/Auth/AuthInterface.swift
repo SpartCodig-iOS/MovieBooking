e02917c0ebdf1ac94089e4df85e1cc7e782c6ec6
Roy <shuwj81@daum.net>
2025-10-14 20:08:37 +0900

    [feat]:  애플로그인  인터페이스 구현 #4

---
+++ MovieBooking/Domain/Repository/Auth/AuthInterface.swift
@@ -0,0 +1,18 @@
+//
+//  AuthInterface.swift
+//  MovieBooking
+//
+//  Created by Wonji Suh  on 10/14/25.
+//
+
+import Foundation
+import AuthenticationServices
+import Supabase
+
+public protocol AuthInterface: Sendable {
+  func idToken(from credential: ASAuthorizationAppleIDCredential) throws -> String
+  func signInWithApple(idToken: String, nonce: String) async throws -> Session
+  func updateDisplayName(_ name: String) async throws
+  func currentSession() async throws -> UserEntity
+  func signInWithAppleOnce(credential: ASAuthorizationAppleIDCredential, nonce: String) async throws -> UserEntity
+}
