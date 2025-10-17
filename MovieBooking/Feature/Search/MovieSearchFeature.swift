5c9cb27c3b57289a9e03858de7a6b2e41e83ff32
김민희 <86248191+minneee@users.noreply.github.com>
2025-10-17 17:55:41 +0900

    [Feature] 영화 검색 구현 (#17)
    * [feat] 영화 리스트 UI 구현
    
    * [feat] 검색 UI 구현
    
    * [feat] 데이터 파싱 구현
    
    * [feat] 영화 리스트 UI 구현
    
    * [feat] 검색 UI 구현
    
    * [feat] 데이터 파싱 구현
    
    * [feat] api 연결
    
    * [feat] 네트워크 에러처리
    
    * [feat] 스크롤 버튼 동작 추가
    
    * [feat] 영화 검색 구현
    
    * [chore] 영화 리스트 합치기 로직 수정

---
+++ MovieBooking/Feature/Search/MovieSearchFeature.swift
@@ -0,0 +1,60 @@
+//
+//  MovieSearchFeature.swift
+//  MovieBooking
+//
+//  Created by 김민희 on 10/16/25.
+//
+
+import ComposableArchitecture
+import Foundation
+
+@Reducer
+struct MovieSearchFeature {
+  @ObservableState
+  struct State: Equatable {
+    var nowPlayingMovies: [Movie] = []
+    var upcomingMovies: [Movie] = []
+    var popularMovies: [Movie] = []
+    var searchText: String = ""
+  }
+
+  enum Action: BindableAction {
+    case updateMovieLists(nowPlaying: [Movie], upcoming: [Movie], popular: [Movie])
+    case binding(BindingAction<State>)
+  }
+
+  var body: some Reducer<State, Action> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+      case let .updateMovieLists(nowPlaying, upcoming, popular):
+        state.nowPlayingMovies = nowPlaying
+        state.upcomingMovies = upcoming
+        state.popularMovies = popular
+        return .none
+
+      case .binding:
+        return .none
+      }
+    }
+  }
+}
+
+
+extension MovieSearchFeature.State {
+  var trimmedKeyword: String {
+    searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+  }
+
+  var aggregatedMovies: [Movie] {
+    Array(Set(nowPlayingMovies + upcomingMovies + popularMovies))
+  }
+
+  var filteredMovies: [Movie] {
+    guard !trimmedKeyword.isEmpty else { return [] }
+
+    return aggregatedMovies.filter {
+      $0.title.localizedCaseInsensitiveContains(trimmedKeyword)
+    }
+  }
+}
